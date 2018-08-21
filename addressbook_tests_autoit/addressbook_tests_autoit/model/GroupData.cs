1bb442e638d782cd2fdee6c8cc567f59f15cfddc
Марина Десятникова <mdesyatnikova@gmail.com>
2018-08-21 17:40:17 +0300

    исправления в тестах для GUI

--- addressbook_tests_autoit/addressbook_tests_autoit/model/GroupData.cs
+++ addressbook_tests_autoit/addressbook_tests_autoit/model/GroupData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace addressbook_tests_autoit
 {
-    public class GroupData: IComparable<GroupData>, IEquatable<GroupData>
+    public class GroupData : IComparable<GroupData>, IEquatable<GroupData>
     {
         public string Name { get; set; }
 
@@ -14,7 +14,16 @@ namespace addressbook_tests_autoit
 
         public bool Equals(GroupData other)
         {
-            return this.Name.Equals(other.Name);
+            if (Object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Name == other.Name;
         }
     }
 }
